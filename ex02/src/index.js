ed08a3180efb7be5d6d2d88f317fb25dd5f7f1d4
TamniVilajet <ermin.kirlic@gmail.com>
2022-03-31 21:03:30 +0200

    Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+
+    var myRes = 75;
+    
+    // Only change code below this line
+    myRes++;
+    // Variable declarations
+
+    
+
+    return myRes;
+}
+
+console.log(main());
+
+module.exports = main;
